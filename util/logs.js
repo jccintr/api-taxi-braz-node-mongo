e6f9d843bbd4a7e5532644537b0582995bee465e
Julio Cesar <jccintr@gmail.com>
2024-11-13 23:44:27 -0300

    corrigido erro ao criar log

--- util/logs.js
+++ util/logs.js
@@ -4,6 +4,6 @@ import PassengerLog from "../models/passengerLog.js";
 export const addLog = async(passenger,action,info) => {
 
     const log = new PassengerLog({ passenger,action,info });
-    await PassengerLog.save();
+    await log.save();
 
 }
